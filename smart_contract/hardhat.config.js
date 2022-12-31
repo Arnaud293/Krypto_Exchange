adaf543e00c1987993c95b6d05fa9ddcd9b8b474
Arnaud293 <arnaudlt293@gmail.com>
2022-12-31 11:17:23 +0100

    config

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli:{
+      url: 'https://eth-goerli.g.alchemy.com/v2/bSIHBXXVnpc3ZSHXIjzMoydxFdtv5cxn',
+      accounts: ['7eaf1bbf5237ee5ec8b335a6b96905eed72ee2b36e095131636e7389d025127e']
+    }
+  }
 };
