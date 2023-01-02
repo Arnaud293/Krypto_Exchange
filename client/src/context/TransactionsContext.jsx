21396f57e9f8810bf1b442315e220621086ede8d
Arnaud293 <arnaudlt293@gmail.com>
2023-01-02 10:33:49 +0100

    connectToWallet, checkIfIsConnected

--- client/src/context/TransactionsContext.jsx
+++ client/src/context/TransactionsContext.jsx
@@ -1,22 +1,62 @@
-import React, {useEffect, useState} from 'react';
-import { ethers } from 'ethers';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
-import { contractABI, contractAddress } from '../utils/constants';
+import { contractABI, contractAddress } from "../utils/constants";
 
 export const TransactionsContext = React.createContext();
 
 const { ethereum } = window;
 
 const getEthereumContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethereum.window);
-    const signer = provider.getSigner();
-    const transationContract = new ethers.Contract(contractAddress, contractABI, signer);
+  const provider = new ethers.providers.Web3Provider(ethereum.window);
+  const signer = provider.getSigner();
+  const transationContract = new ethers.Contract(
+    contractAddress,
+    contractABI,
+    signer
+  );
 };
 
 export const TransactionsProvider = ({ children }) => {
-    return (
-        <TransactionsContext.Provider value={{value: "test"}}>
-            {children}
-        </TransactionsContext.Provider>
-    )
-}
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) return alert("please install metamask");
+      const account = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.log("No account found");
+      }
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No Ethereum object !");
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("please install metamask");
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("No Ethereum object !");
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <TransactionsContext.Provider value={{ connectWallet, currentAccount }}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+};
