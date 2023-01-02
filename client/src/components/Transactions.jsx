c515b31ed56bfa74e4cafde875f1d429c61b3402
Arnaud293 <arnaudlt293@gmail.com>
2023-01-02 16:22:23 +0100

    render real transactions and remove dummyData

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -46,7 +46,7 @@ const TransactionCard = ({
 };
 
 const Transactions = () => {
-  const { currentAccount } = useContext(TransactionsContext);
+  const { currentAccount, transactions } = useContext(TransactionsContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
@@ -61,7 +61,7 @@ const Transactions = () => {
           </h3>
         )}
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, index) => (
+          {transactions.reverse().map((transaction, index) => (
             <TransactionCard key={index} {...transaction} />
           ))}
         </div>
